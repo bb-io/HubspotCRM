1b36ce481c9f66e9b9d891a9ea012e0ab7a9478d
mathijs-bb <mathijs.sonnemans@blackbird.io>
2023-05-20 17:26:52 +0200

    removed console

--- ConsoleApp1/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿using ConsoleApp1;
-using Newtonsoft.Json;
-
-// See https://aka.ms/new-console-template for more information
-var serialized = JsonConvert.DeserializeObject<Test>("{\"name\": \"foo\"}");
-var deserialized = JsonConvert.SerializeObject(serialized);
-Console.WriteLine(deserialized);
