caabdff05690f81ec29c7c78ec6999369c4ee36e
bohdanm-bb <bohdan.makarenko@blackbird.io>
2023-10-27 16:38:16 +0300

    Artifactory

---
+++ Apps.Hubspot/Apps.Hubspot/ApplicationConstants.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apps.Hubspot.Crm
+{
+    public class ApplicationConstants
+    {
+        public const string ClientId = "#{HUBSPOTCRM_CLIENT_ID}";
+
+        public const string Scope = "#{HUBSPOTCRM_SCOPE}#";
+
+        public const string ClientSecret = "#{HUBSPOTCRM_SECRET}#";
+
+        public const string BridgeServiceUrl = "#{HUBSPOTCRM_BRIDGE_SERVICE_TEMP}#"; // TO DO: remove
+
+        public const string BlackbirdToken = "#{HUBSPOTCRM_BLACKBIRD_TOKEN}#"; // bridge validates this token
+    }
+}
--- Apps.Hubspot/Apps.Hubspot/Auth/OAuth2/OAuth2AuthorizeService.cs
+++ Apps.Hubspot/Apps.Hubspot/Auth/OAuth2/OAuth2AuthorizeService.cs
@@ -1,23 +1,29 @@
 ﻿using Apps.Hubspot.Crm.Constants;
+using Blackbird.Applications.Sdk.Common;
 using Blackbird.Applications.Sdk.Common.Authentication.OAuth2;
+using Blackbird.Applications.Sdk.Common.Invocation;
 using Blackbird.Applications.Sdk.Utils.Extensions.String;
 using Microsoft.AspNetCore.WebUtilities;
 
 namespace Apps.Hubspot.Crm.Auth.OAuth2;
 
-public class OAuth2AuthorizeService : IOAuth2AuthorizeService
+public class OAuth2AuthorizeService : BaseInvocable, IOAuth2AuthorizeService
 {
+    public OAuth2AuthorizeService(InvocationContext invocationContext) : base(invocationContext)
+    {
+    }
+
     public string GetAuthorizationUrl(Dictionary<string, string> values)
     {
-        string bridgeOauthUrl = $"{ApplicationConstants.BridgeUri}/oauth";
+        string bridgeOauthUrl = $"{InvocationContext.UriInfo.BridgeServiceUrl.ToString().TrimEnd('/')}/oauth";
         var parameters = new Dictionary<string, string>
         {
             { "client_id", ApplicationConstants.ClientId },
-            { "redirect_uri", $"{ApplicationConstants.BridgeUri.TrimEnd('/')}/AuthorizationCode" },
+            { "redirect_uri", $"{InvocationContext.UriInfo.BridgeServiceUrl.ToString().TrimEnd('/')}/AuthorizationCode" },
             { "scope", ApplicationConstants.Scope },
             { "state", values["state"] },
             { "authorization_url", Urls.Authorize},
-            { "actual_redirect_uri", ApplicationConstants.RedirectUri },
+            { "actual_redirect_uri", InvocationContext.UriInfo.AuthorizationCodeRedirectUri.ToString() },
         };
 
         return QueryHelpers.AddQueryString(bridgeOauthUrl, parameters);
