a182c769f1d60c2ea6a33f3c3bc200b3f24075b7
vitalii-bezuhlyi <vitalii.bezuhlyi@blackbird.io>
2024-01-26 20:38:01 +0200

    Added dynamic date handler to On contact association changed webhook

---
+++ Apps.Hubspot/Webhooks/Inputs/ContactAssociationChangedInput.cs
@@ -0,0 +1,13 @@
+﻿using Apps.Hubspot.Crm.DataSourceHandlers.EnumHandlers;
+using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Dynamic;
+
+namespace Apps.Hubspot.Crm.Webhooks.Inputs
+{
+    public class ContactAssociationChangedInput
+    {
+        [Display("Association type")]
+        [DataSource(typeof(ContactAssociationEnumDataHandler))]
+        public string? AssociationType { get; set; }
+    }
+}
