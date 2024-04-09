de86778f1cf047997e6d0fd99ab8eb605eef144c
bZverok <137277669+bZverok@users.noreply.github.com>
2024-04-09 11:43:38 +0300

    added categories

--- Apps.Hubspot/HubspotApplication.cs
+++ Apps.Hubspot/HubspotApplication.cs
@@ -2,14 +2,21 @@
 using Blackbird.Applications.Sdk.Common;
 using Blackbird.Applications.Sdk.Common.Authentication.OAuth2;
 using Blackbird.Applications.Sdk.Common.Invocation;
+using Blackbird.Applications.Sdk.Common.Metadata;
 
 namespace Apps.Hubspot.Crm;
 
-public class HubspotApplication : BaseInvocable, IApplication
+public class HubspotApplication : BaseInvocable, IApplication, ICategoryProvider
 {
     private string _name;
     private readonly Dictionary<Type, object> _typesInstances;
 
+    public IEnumerable<ApplicationCategory> Categories
+    {
+        get => [ApplicationCategory.Marketing, ApplicationCategory.Crm];
+        set { }
+    }
+    
     public HubspotApplication(InvocationContext invocationContext) : base(invocationContext)
     {
         _name = "Hubspot CRM";
